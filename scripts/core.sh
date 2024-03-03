282a89e173a6085bec526397a04555885109376d
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-03 14:41:17 +0700

    update core

--- scripts/core.sh
+++ scripts/core.sh
@@ -4,8 +4,6 @@ touch ~/.no_auto_tmux;
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
 source ~/.bashrc
-git clone https://github.com/semperai/arbius.git &&
-cd arbius/miner
 nvm install --lts
 npm install -g yarn
 yarn
