3f45521018c3d35bf6e0277c07513fb668dc0ff0
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-03 15:22:52 +0700

    update core setup

--- miner/scripts/core.sh
+++ miner/scripts/core.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# Download and install nvm using curl
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install --lts
 
-# Ensure nvm is available in the current shell session
-source ~/.bashrc
+npm install -g yarn
 
-# Install the latest LTS version of Node.js
-# nvm install --lts
+yarn
 
-# # Install Yarn globally
-# npm install -g yarn
-
-# # Install project dependencies using Yarn
-# yarn
-
-# # Install pm2 globally
-# npm install -g pm2
+npm install -g pm2
