e8e1f874efc88e6616c797f8ffc159c657b13414
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-03 14:41:53 +0700

    update core

--- scripts/core.sh
+++ scripts/core.sh
@@ -3,7 +3,8 @@
 touch ~/.no_auto_tmux;
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
-source ~/.bashrc
+source ~/.bashrc &&
+sleep 4
 nvm install --lts
 npm install -g yarn
 yarn
