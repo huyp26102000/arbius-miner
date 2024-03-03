3458e957adc35ac8385f97a4f98fdca080e3aa7f
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-03 15:15:46 +0700

    test setup

--- miner/scripts/core.sh
+++ miner/scripts/core.sh
@@ -4,17 +4,16 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 # Ensure nvm is available in the current shell session
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source ~/.bashrc
 
 # Install the latest LTS version of Node.js
-nvm install --lts
+# nvm install --lts
 
-# Install Yarn globally
-npm install -g yarn
+# # Install Yarn globally
+# npm install -g yarn
 
-# Install project dependencies using Yarn
-yarn
+# # Install project dependencies using Yarn
+# yarn
 
-# Install pm2 globally
-npm install -g pm2
+# # Install pm2 globally
+# npm install -g pm2
