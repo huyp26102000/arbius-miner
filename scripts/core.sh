118f27d32c4604f32130e45c6e21317cc05116b2
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-03 14:40:32 +0700

    update core setup

--- scripts/core.sh
+++ scripts/core.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-# Name of the tmux session
-session_name="ipfs"
+touch ~/.no_auto_tmux;
 
-# Name of the script file to run
-script_to_run="ipfs.sh"
-
-# Create a new tmux session if it doesn't exist, otherwise attach to the existing one
-tmux has-session -t $session_name 2>/dev/null
-
-if [ $? != 0 ]; then
-    # Start a new tmux session if it doesn't exist
-    tmux new-session -d -s $session_name
-
-    # Split the window horizontally
-    tmux split-window -h
-fi
-
-# Send a command to the first pane to run the script
-tmux send-keys -t $session_name:0.0 "bash $script_to_run" C-m
-
-# Attach to the tmux session
-tmux attach-session -t $session_name
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
+source ~/.bashrc
+git clone https://github.com/semperai/arbius.git &&
+cd arbius/miner
+nvm install --lts
+npm install -g yarn
+yarn
+npm i -g pm2
