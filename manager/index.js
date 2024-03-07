c0d858b7935043309844867909e666d4036cf5ac
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-07 15:01:30 +0700

    update claim action

--- manager/index.js
+++ manager/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const pm2 = require('pm2');
+const pm2 = require("pm2");
 const app = express();
 require("dotenv").config();
-
+const PORT = 3001;
 app.get("/", (req, res) => {
   res.send("Arbius manager api");
 });
+const actions = ["claim", "start", "start-automine"];
+
 app.get("/pm2/jobs", (req, res) => {
   pm2.connect((err) => {
     if (err) {
@@ -20,13 +22,63 @@ app.get("/pm2/jobs", (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
         return;
       }
-      console.log(processes);
-      res.json({ processes });
+      res.json(
+        actions.map((e) => {
+          const process = processes.find((j) => j?.name == e);
+          return {
+            name: e,
+            id: process?.pm_id,
+            status: process?.pm2_env?.status == "online",
+          };
+        })
+      );
       pm2.disconnect();
     });
   });
 });
+app.get("/claim", (req, res) => {
+  const newStatus = req?.query?.status;
+  if (newStatus) {
+    try {
+      pm2.connect((err) => {
+        if (err) {
+          console.error(err);
+          process.exit(2);
+        }
+        pm2.restart("claim", (restartErr) => {
+          if (restartErr) {
+            console.error(restartErr);
+            res.send(restartErr);
+          }
+          pm2.disconnect();
+        });
+      });
+      res.send("Successfully update status");
+    } catch (error) {
+      res.send(restartErr, 404);
+    }
+  } else {
+    try {
+      pm2.connect((err) => {
+        if (err) {
+          console.error(err);
+          process.exit(2);
+        }
+        pm2.stop("claim", (restartErr) => {
+          if (restartErr) {
+            console.error(restartErr);
+            res.send(restartErr);
+          }
+          pm2.disconnect();
+        });
+      });
+      res.send("Successfully update status");
+    } catch (error) {
+      res.send(restartErr, 404);
+    }
+  }
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at http://localhost:${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
