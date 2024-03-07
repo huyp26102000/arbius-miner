c36c8fab67bd44b41e514746a59faad051377f8a
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-07 13:03:43 +0700

    fix port

--- manager/index.js
+++ manager/index.js
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
