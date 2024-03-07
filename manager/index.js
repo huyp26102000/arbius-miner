78f26a451a93b64e1bedbfeb4075c94e67a98cd2
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-07 13:01:46 +0700

    add api

---
+++ manager/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+// Define a route
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+// Start the server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
