8e86bda06996016d4bc741fea690232afd9b4f45
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-07 13:39:35 +0700

    update get api

--- manager/index.js
+++ manager/index.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const pm2 = require('pm2');
 const app = express();
 require("dotenv").config();
 
-// Define a route
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  res.send("Arbius manager api");
+});
+app.get("/pm2/jobs", (req, res) => {
+  pm2.connect((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    pm2.list((err, processes) => {
+      if (err) {
+        console.error(err);
+        pm2.disconnect();
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+      console.log(processes);
+      res.json({ processes });
+      pm2.disconnect();
+    });
+  });
 });
 
-// Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
