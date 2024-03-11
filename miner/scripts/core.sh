2a039342291aa7760e442d0fce5c3b9bb8a47ba5
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-03-12 02:01:36 +0700

    update investigation

--- miner/scripts/core.sh
+++ miner/scripts/core.sh
@@ -2,10 +2,6 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-echo "NVM directory: $NVM_DIR"
 nvm install --lts
-NVM_VERSION=$(nvm --version)
 npm install -g yarn
 npm install -g pm2
